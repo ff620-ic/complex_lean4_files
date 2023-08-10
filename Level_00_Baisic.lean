51d9214321990fd0cccc935ad915d14938a98314
KathyS01 <ks2021@ic.ac.uk>
2023-08-10 11:19:46 +0000

    def

--- Level_00_Baisic.lean
+++ Level_00_Baisic.lean
@@ -1,5 +1,20 @@
-structure Point (α : ℝ) where
-  x : ℝ
-  y : ℝ
+structure complex where
+re : Nat
+im : Nat
 
-notation `ℂ` := complex
+notation "ℂ" => complex
+
+#check complex.mk 10 20
+
+def zero : ℂ := ⟨0, 0⟩
+
+instance : HasZero ℂ := ⟨zero⟩
+
+def one : ℂ := ⟨1, 0⟩
+
+instance : HasOne ℂ := ⟨one⟩
+
+def add (z w : ℂ) : ℂ := ⟨z.re + w.re, z.im + w.im⟩
+
+def mul (z w : ℂ) : ℂ := ⟨z.re * w.re - z.im * w.im, z.re * w.im + z.im * w.re⟩
+
