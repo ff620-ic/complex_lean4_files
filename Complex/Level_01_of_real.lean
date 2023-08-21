e75ff681423d6b5e112a2ef3fb1f175b1a04056f
KathyS01 <ks2021@ic.ac.uk>
2023-08-21 15:54:08 +0000

    01

--- Complex/Level_01_of_real.lean
+++ Complex/Level_01_of_real.lean
@@ -4,4 +4,46 @@ namespace complex
 
 def of_real (r : ℝ) : ℂ := ⟨r, 0⟩
 
-instance : has_coe ℝ ℂ := ⟨of_real⟩
+instance : Coe ℝ ℂ := ⟨of_real⟩
+
+@[simp]
+theorem of_real_re (r : ℝ) : (r : ℂ).re = r := 
+  rfl
+
+--@[simp, norm_cast] lemma of_real_re (r : ℝ) : (r : ℂ).re = r :=
+--begin
+  --refl
+--end
+
+@[simp]
+theorem of_real_im (r : ℝ) : (r : ℂ).im = 0 :=
+  rfl
+
+--@[simp, norm_cast] lemma of_real_im (r : ℝ) : (r : ℂ).im = 0 := rfl
+
+attribute [simp] complex.ext_iff
+
+@[simp]
+theorem of_real_inj {r s : ℝ} : (r : ℂ) = s ↔ r = s := 
+  rfl
+
+
+@[simp]
+theorem of_real_zero : ((0 : ℝ) : ℂ) = 0 := 
+  rfl
+
+--@[simp, norm_cast] lemma of_real_zero : ((0 : ℝ) : ℂ) = 0 := rfl
+
+@[simp]
+theorem of_real_eq_zero {r : ℝ} : (r : ℂ) = 0 ↔ r = 0 :=
+  rfl
+
+@[simp, norm_cast] theorem of_real_one : ((1 : ℝ) : ℂ) = 1 := 
+  rfl
+
+@[simp, norm_cast] lemma of_real_add (r s : ℝ) : ((r + s : ℝ) : ℂ) = r + s :=
+  rfl
+
+@[simp, norm_cast] lemma of_real_bit0 (r : ℝ) : ((bit0 r : ℝ) : ℂ) = bit0 r :=
+  rfl
+
