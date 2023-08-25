a4ea8dd1ecae1d3b8aea8e93777dc64da776bc48
Fanglan Feng <fanglan.feng21@imperial.ac.uk>
2023-08-25 12:01:40 +0000

    statement added. errors fixed.

--- Complex/Level_00_Basic.lean
+++ Complex/Level_00_Basic.lean
@@ -1,49 +1,120 @@
+/-
+Copyright (c) 2020 The Xena project. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kevin Buzzard.
+Thanks: Imperial College London, leanprover-community
+-/
+
+-- Import everything about the real numbers
 import Mathlib.Data.Real.Basic
 
+/-!
+# The complex numbers
+
+We define the complex numbers, and prove that they are a ring.
+
+We also "extract some basic API" (e.g. we prove that
+two complex numbers are equal iff they have the same
+real and imaginary parts)
+
+This file has no `sorry`s in. All of the other levels:
+
+`Level_01_of_real.lean`
+`Level_02_I.lean`
+`Level_03_conj.lean`
+`Level_04_norm_sq.lean`
+`Level_05_field.lean`
+`Level_06_alg_closed.lean`
+
+have sorrys, indicating puzzles to be solved.
+
+# Main Definitions
+
+`zero` : the complex number 0
+`one` : the complex number 1
+`add` -- addition of two complex numbers
+`neg` -- negation of a complex number
+`mul` -- multiplication of two complex numbers
+
+# Main Theorem
+
+`comm_ring` : The complex numbers are a commutative ring.
+
+-/
+
+/-- A complex number is defined to be a structure consisting of two real
+  numbers, the real part and the imaginary part of the complex numberd. -/
 @[ext]
 structure complex where
   re : ℝ
   im : ℝ
 
+--@[ext] annotation automatically tells lean two instances are equal when their components are equal.
 #check complex.ext
 
+-- Let's use the usual notation for the complex numbers
 notation "ℂ" => complex
 
 #check complex.mk 10 20
 
+-- You make the complex number with real part 3 and imaginary part 4 like this:
 example : ℂ := complex.mk 3 4
 
+-- Or like this:
 example : ℂ := ⟨3, 4⟩
 
+-- They all give the same complex number.
 example : complex.mk 3 4 = ⟨3, 4⟩ := by rfl
+
+-- All of our definitions, like `zero` and `one`, will all
+-- live in the `complex` namespace.
 
 namespace complex
 
+-- If you have a complex number, then you can get its real and 
+-- imaginary parts with the `re` and `im` functions.
+-- Note that re(mk 3 4) = 3 is no longer available.
+
 example : ℝ := (mk 3 4).re
+
+-- Before we start making the basic 
+-- We now prove the basic theorems and make the basic definitions for
+-- complex numbers. For example, we will define addition and multiplication on
+-- the complex numbers, and prove that it is a commutative ring.
+-- TODO fix
+
+/-! # Defining the ring structure on ℂ -/
+
+-- Our main goal is to prove that the complexes are a ring. Let's
+-- define the structure first; the zero, one, addition and multiplication
+-- on the complexes. 
+
+/-! ## zero (0) -/
+
+/-- The complex number with real part 0 and imaginary part 0 -/
 
 instance : Zero complex :=
   ⟨⟨0, 0⟩⟩
 
-instance : One complex :=
-  ⟨⟨1, 0⟩⟩
-
-instance : Neg complex :=
-  ⟨fun x ↦ ⟨-x.re, -x.im⟩⟩
-
-instance : Add complex :=
-  ⟨fun x y ↦ ⟨x.re + y.re, x.im + y.im⟩⟩
-
-instance : Mul complex :=
-  ⟨fun x y ↦ ⟨x.re * y.re - x.im * y.im, x.re * y.im + x.im * y.re⟩⟩
-
+-- Let's prove the two basic properties, both of which are true by definition,
+-- and then tag them with the appropriate attributes.
 @[simp]
 theorem zero_re : (0 : complex).re = 0 :=
   rfl
 
 @[simp]
 theorem zero_im : (0 : complex).im = 0 :=
-  rfl
+  rfl  
 
+/-! ## one (1) -/
+
+-- Now let's do the same thing for 1.
+
+/-- The complex number with real part 1 and imaginary part 0 -/
+instance : One complex :=
+  ⟨⟨1, 0⟩⟩
+
+-- name the basic properties and tag them with `simp`
 @[simp]
 theorem one_re : (1 : complex).re = 1 :=
   rfl
@@ -52,6 +123,17 @@ theorem one_re : (1 : complex).re = 1 :=
 theorem one_im : (1 : complex).im = 0 :=
   rfl
 
+
+/-! ## add (+) -/
+
+-- Now let's define addition
+
+/-- addition `z+w` of complex numbers -/
+
+instance : Add complex :=
+  ⟨fun x y ↦ ⟨x.re + y.re, x.im + y.im⟩⟩
+
+-- basic properties
 @[simp]
 theorem add_re (x y : complex) : (x + y).re = x.re + y.re :=
   rfl
@@ -60,6 +142,15 @@ theorem add_re (x y : complex) : (x + y).re = x.re + y.re :=
 theorem add_im (x y : complex) : (x + y).im = x.im + y.im :=
   rfl
 
+/-! ## neg (-) -/
+
+-- negation
+
+/-- The negation `-z` of a complex number `z` -/
+instance : Neg complex :=
+  ⟨fun x ↦ ⟨-x.re, -x.im⟩⟩
+
+-- how neg interacts with re and im
 @[simp]
 theorem neg_re (x : complex) : (-x).re = -x.re :=
   rfl
@@ -68,6 +159,15 @@ theorem neg_re (x : complex) : (-x).re = -x.re :=
 theorem neg_im (x : complex) : (-x).im = -x.im :=
   rfl
 
+/-! ## mul (*) -/
+
+-- multiplication
+
+/-- Multiplication `z*w` of two complex numbers -/
+instance : Mul complex :=
+  ⟨fun x y ↦ ⟨x.re * y.re - x.im * y.im, x.re * y.im + x.im * y.re⟩⟩
+
+-- how `mul` reacts with `re` and `im`
 @[simp]
 theorem mul_re (x y : complex) : (x * y).re = x.re * y.re - x.im * y.im :=
   rfl
@@ -76,6 +176,26 @@ theorem mul_re (x y : complex) : (x * y).re = x.re * y.re - x.im * y.im :=
 theorem mul_im (x y : complex) : (x * y).im = x.re * y.im + x.im * y.re :=
   rfl
 
+/-! ## Example of what `simp` can now do
+
+example (a b c : ℂ) :
+  re(a*(b+c)) = re(a) * (re(b) + re(c)) - im(a) * (im(b) + im(c)) :=
+begin
+  simp,
+end
+
+-/
+
+/-! # Theorem:  The complex numbers are a commutative ring
+
+Proof: we've defined all the structure, and every axiom can be checked by
+reducing it to checking real and imaginary parts with `ext`, expanding
+everything out with `simp`, and then using the fact that the real numbers are
+a commutative ring (which we already know)
+
+-/
+
+/-- The complex numbers are a commutative ring -/
 instance instCommRing : CommRing ℂ where
   zero := 0
   one := 1
@@ -129,3 +249,23 @@ instance : Nontrivial complex := by
 
 end complex
 
+/-!
+
+# some last comments on the `simp` tactic
+
+Some equalities, even if obvious, had to be given names, because we want `simp`
+to be able to use them. In short, the `simp` tactic tries to solve
+goals of the form A = B, when `refl` doesn't work (i.e. the goals are
+not definitionally equal) but when any mathematician would be able
+to simplify A and B via "obvious" steps such as `0 + x = x` or
+`⟨z.re, z.im⟩ = z`. These things are sometimes not true by definition,
+but they should be tagged as being well-known ways to simplify an equality.
+When building our API for the complex numbers, if we prove a theorem of the
+form `A = B` where `B` is a bit simpler than `A`, we should probably
+tag it with the `@[simp]` attribute, so `simp` can use it.
+
+Note: `simp` does *not* prove "all simple things". It proves *equalities*.
+It proves `A = B` when, and only when, it can do it by applying 
+its "simplification rules", where a simplification rule is simply a proof
+of a theorem of the form `A = B` and `B` is simpler than `A`.  
+-/
--- Complex/Level_01_of_real.lean
+++ Complex/Level_01_of_real.lean
@@ -1,11 +1,50 @@
+/-
+Copyright (c) 2020 The Xena project. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kevin Buzzard.
+Thanks: Imperial College London, leanprover-community
+-/
+
+-- import the definition and basic properties of ℂ
 import Complex.Level_00_Basic
+
+/-! # Level 1 : the map from ℝ to ℂ
+
+This file sets up the coercion from the reals to the complexes,
+sending `r` to `⟨r, 0⟩`. Mathematically it is straightforward.
+
+All the proofs below are sorried. You can try them in tactic mode
+by replacing `sorry` with `begin sorry end` and then starting to write 
+tactics in the `begin end` block.
+
+-/
 
 namespace complex
 
+-- fill in the definition of the map below,
+-- sending the real number r to the complex number ⟨r, 0⟩
+
+/-- The canonical map from ℝ to ℂ. -/
 def of_real (r : ℝ) : ℂ := ⟨r, 0⟩
 
+/-
+We make this map into a *coercion*, which means that if `(r : ℝ)` is a real
+number, then `(r : ℂ)` or `(↑r : ℂ)` will indicate the corresponding
+complex number with no imaginary part. This is the notation we shall
+use in our `simp` lemmas.
+-/
+
+/-- The coercion from ℝ to ℂ sending `r` to the complex number `⟨r, 0⟩` -/
 instance : Coe ℝ ℂ := ⟨of_real⟩
 
+/-
+As usual, we need to train the `simp` tactic. But we also need to train
+the `norm_cast` tactic. The `norm_cast` tactic enables Lean to prove
+results like r^2=2*s for reals `r` and `s`, if it knows that
+`(r : ℂ)^2 = 2*(s : ℂ)`. Such results are intuitive for matheamticians
+but involve "invisible maps" in Lean. But unfortunately we haven’t figure
+out how to use norm_cast in following theorems and lemmas.
+-/
 @[simp]
 lemma of_real_re (r : ℝ) : ((r : ℝ) : ℂ).re = r :=
   rfl
@@ -18,20 +57,27 @@ lemma of_real_re (r : ℝ) : ((r : ℝ) : ℂ).re = r :=
 @[simp]
 lemma of_real_im (r : ℝ) : (r : ℂ).im = 0 :=
   rfl
---@[simp, norm_cast] lemma of_real_im (r : ℝ) : (r : ℂ).im = 0 := rfl
-
+-- The map from the reals to the complexes is injective, something we
+-- write in iff form so `simp` can use it; `simp` also works on `iff` goals.
 attribute [simp] complex.ext_iff
 
 @[simp]
 theorem of_real_inj {r s : ℝ} : (r : ℂ) = s ↔ r = s := 
   ⟨congr_arg re, congr_arg _⟩
 
+/-
+We now go through all the basic constants and constructions we've defined so
+far, namely 0, 1, +, -, *, and tell the simplifier how they behave with respect
+to this new function. 
+-/
+
+/-! ## zero -/
+
 @[simp]
 theorem of_real_zero : ((0 : ℝ) : ℂ) = 0 := 
   rfl
 
---@[simp, norm_cast] lemma of_real_zero : ((0 : ℝ) : ℂ) = 0 := rfl
-
+--equals zero iff r=0
 @[simp]
 theorem of_real_eq_zero {r : ℝ} : (r : ℂ) = 0 ↔ r = 0 :=
   of_real_inj
@@ -39,22 +85,27 @@ theorem of_real_eq_zero {r : ℝ} : (r : ℂ) = 0 ↔ r = 0 :=
 theorem of_real_ne_zero {r : ℝ} : (r : ℂ) ≠ 0 ↔ r ≠ 0 :=
   not_congr of_real_eq_zero
 
+/-! ## one -/
 @[simp]
 theorem of_real_one : ((1 : ℝ) : ℂ) = 1 := 
   rfl
 
+/-! ## add -/
 @[simp]
 theorem of_real_add (r s : ℝ) : ((r + s : ℝ) : ℂ) = r + s := by
   simp
 
+/-! ## neg -/
 @[simp]
 lemma of_real_neg (r : ℝ) : ((-r : ℝ) : ℂ) = -r := by
   simp
 
+/-! ## mul -/
 @[simp]
 lemma of_real_mul (r s : ℝ) : ((r * s : ℝ) : ℂ) = r * s := by
   simp
 
+/-- The canonical ring homomorphism from ℝ to ℂ -/
 def Of_real : ℝ →+* ℂ :=
 { toFun := of_real, -- use the coercion from ℝ to ℂ
   map_zero' := of_real_zero,
@@ -69,5 +120,7 @@ def Of_real : ℝ →+* ℂ :=
 
 @[simp] lemma of_real_bit1 (r : ℝ) : ((bit1 r : ℝ) : ℂ) = bit1 r :=
   rfl
+
+bit0 and bit1 deprecated.
 -/
 end complex
