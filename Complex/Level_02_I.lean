04c4afc9d7d4d2a7842ec5fe1eccb7b921ba85ea
KathyS01 <ks2021@ic.ac.uk>
2023-08-25 08:55:18 +0000

    level2 error exist

--- Complex/Level_02_I.lean
+++ Complex/Level_02_I.lean
@@ -8,4 +8,12 @@ def I : ℂ := ⟨0, 1⟩
 
 @[simp] lemma I_im : I.im = 1 := rfl
 
+@[simp] lemma I_mul_I : I * I = -1 := rfl
 
+lemma mk_eq_add_mul_I (a b : ℝ) : (⟨a, b⟩ : ℂ) = a + b * I := rfl
+
+@[simp] lemma re_add_im (z : ℂ) : (z.re : ℂ) + z.im * I = z := rfl
+
+lemma I_ne_zero : (I : ℂ) ≠ 0 := simp[I_im, I_re, zero_re, zero_im]
+
+end complex
